8f538adf7957b5270d836a76bd95109a8faac634
lukasst <lukas.steiger@hotmail.com>
2015-11-23 14:30:13 +0100

    add theming

--- Gadgeothek-Admin-App/MainWindow.xaml.cs
+++ Gadgeothek-Admin-App/MainWindow.xaml.cs
@@ -12,17 +12,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+using MahApps.Metro.Controls;
 
 namespace Gadgeothek_Admin_App
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            var url = "http://localhost:8080";
+            LibraryAdminService las = new LibraryAdminService(url);
+            List<Customer> lesCustomers = las.GetAllCustomers();
+            lesCustomers.ForEach(x => printCustomer(x));
+        }
+
+        private void printCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Email);
         }
     }
 }
