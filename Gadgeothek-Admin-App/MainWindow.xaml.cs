6f0343bcc42ed024522ee27af7c77ee6553b0027
lukasst <lukas.steiger@hotmail.com>
2015-11-25 15:39:21 +0100

    remove unused code & simplify xml

--- Gadgeothek-Admin-App/MainWindow.xaml.cs
+++ Gadgeothek-Admin-App/MainWindow.xaml.cs
@@ -1,21 +1,5 @@
 ﻿using ch.hsr.wpf.gadgeothek.service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Configuration;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ch.hsr.wpf.gadgeothek.domain;
-using ch.hsr.wpf.gadgeothek.service;
 using MahApps.Metro.Controls;
 
 namespace Gadgeothek_Admin_App
