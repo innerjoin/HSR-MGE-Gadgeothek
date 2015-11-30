3a2e33e40e17ae61836e6f98efbe7769974ba354
lukasst <lukas.steiger@hotmail.com>
2015-11-30 13:08:57 +0100

    first binding stuff

--- Gadgeothek-Admin-App/MainWindow.xaml.cs
+++ Gadgeothek-Admin-App/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using ch.hsr.wpf.gadgeothek.service;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using ch.hsr.wpf.gadgeothek.service;
 using System.Configuration;
+using ch.hsr.wpf.gadgeothek.domain;
 using MahApps.Metro.Controls;
 
 namespace Gadgeothek_Admin_App
@@ -9,14 +14,59 @@ namespace Gadgeothek_Admin_App
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        LibraryAdminService service = new LibraryAdminService(ConfigurationManager.AppSettings["server"]);
         /// <summary>
         /// 
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            LibraryAdminService service = new LibraryAdminService(ConfigurationManager.AppSettings["server"]);
-            gadgetsDataGridView.ItemsSource = service.GetAllGadgets();
+            
+            List<Gadget> data = service.GetAllGadgets();
+            ObservableCollection<Gadget> sourceCollection = new ObservableCollection<Gadget>(data);
+
+            sourceCollection.CollectionChanged +=
+                new NotifyCollectionChangedEventHandler(DataGrid_CollectionChanged);
+            gadgetsDataGridView.ItemsSource = sourceCollection;
+
+        }
+
+        void DataGrid_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            bool healthy = false;
+            if (sender.GetType() == typeof (ObservableCollection<Gadget>))
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Remove:
+                        foreach(Gadget gadgetToRemove in e.OldItems)
+                        {
+                            healthy = service.DeleteGadget(gadgetToRemove);
+                        }
+                        break;
+                    case NotifyCollectionChangedAction.Add:
+                        foreach (Gadget newGadget in e.NewItems)
+                        {
+                            healthy = newGadget.Manufacturer != null 
+                                && newGadget.Name != null 
+                                && service.AddGadget(newGadget);
+                        }
+                        break;
+                    case NotifyCollectionChangedAction.Replace:
+                        break;
+                    case NotifyCollectionChangedAction.Move:
+                        break;
+                    case NotifyCollectionChangedAction.Reset:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            if (!healthy)
+            {
+                // TODO prevent grid change
+            }
         }
     }
 }
